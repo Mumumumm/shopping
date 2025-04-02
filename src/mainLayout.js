6dfa6232805eafec17d78913bec724bdd26afbbf
김준홍 <rlawnsghd434343@gmail.com>
2025-04-02 14:57:41 +0900

    Link 추가

--- src/mainLayout.js
+++ src/mainLayout.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export default function MainLayout(props) {
 
     return (
         <>
             <h1>{props.title}</h1>
+            <Link to='/product'>상품보기</Link>
+            <Link to='/shpping_cart'>장바구니</Link>
+            <Link to='/payment'>결제금액보기</Link>
         </>
     );
 }
