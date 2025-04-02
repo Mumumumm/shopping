ef327670051087bb5e328b47d5946df7d592c1f1
김준홍 <rlawnsghd434343@gmail.com>
2025-04-02 17:40:39 +0900

    Cart에 담겨야하는 내용 적어보기

--- src/shopping_cart.js
+++ src/shopping_cart.js
@@ -1,10 +1,15 @@
+
+
 export default function ShopCart() {
 
     return (
         <>
             <h2>Shopping Cart 🛒</h2>
             <ul>
-
+                <li>Product A - 100원 X 갯수
+                    <button type="button">Remove</button>
+                    <input type="number" />
+                </li>
             </ul>
         </>
     );
