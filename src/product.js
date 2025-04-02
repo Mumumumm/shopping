8ecc396de5d3e90ca611406c68888035f1760271
김준홍 <rlawnsghd434343@gmail.com>
2025-04-02 15:19:52 +0900

    product  event 기능구현

--- src/product.js
+++ src/product.js
@@ -2,7 +2,19 @@ export default function Product() {
 
     return (
         <>
+            <ul>
+                <li>Product A - 100원
+                    <button type="button" onClick={(e) => { alert('상품이 추가되었습니다.') }}>Add to Cart</button>
+                </li>
 
+                <li>Product B - 200원
+                    <button type="button" onClick={(e) => { alert('상품이 추가되었습니다.') }}>Add to Cart</button>
+                </li>
+
+                <li>Product C - 300원
+                    <button type="button" onClick={(e) => { alert('상품이 추가되었습니다.') }}>Add to Cart</button>
+                </li>
+            </ul>
         </>
     );
 }
