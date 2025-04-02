4f61439c2c26705634ef534bdba7e904ecdfb56a
김준홍 <rlawnsghd434343@gmail.com>
2025-04-02 15:02:05 +0900

    welcome 컴포넌트 index route 설정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from './mainLayout';
+import Welcome from './welcome';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<MainLayout title="Shopping"/>}>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path='/' element={<MainLayout title="Shopping" />}>
+          <Route index element={<Welcome />}></Route>
+          <Route></Route>
+          <Route></Route>
         </Route>
       </Routes>
     </div>
